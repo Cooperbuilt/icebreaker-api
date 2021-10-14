20b8296eadce54c5604a1c16474917611bc96ff9
Evan Cooper <evcooper@wayfair.com>
2021-10-14 17:36:47 -0400

    Adding api

--- contentfull-api.js
+++ contentfull-api.js
@@ -9,36 +9,83 @@ const client = contentful.createClient({
   accessToken: process.env.CONTENTFUL,
 });
 
-/**
- *
- * @param {Object} object
- */
-const updateIcebreaker = async function ({ itemID, teamName }) {
-  client
-    .getSpace("<space_id>")
-    .then((space) => space.getEnvironment("<environment_id>"))
-    .then((environment) => environment.getEntry("<entry_id>"))
+function getArrayRandomElement(arr) {
+  if (arr && arr.length) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+}
+
+async function getSpace(spaceId, contentfulApi = client) {
+  return contentfulApi.getSpace(spaceId);
+}
+
+async function getEnvironment(envId, contentfulApi) {
+  return contentfulApi.getEnvironment(envId);
+}
+
+async function getEntries(contentfulApi) {
+  return contentfulApi.getEntries();
+}
+
+async function getEntry(entryId, contentfulApi) {
+  return contentfulApi.getEntry(entryId);
+}
+
+async function updateIcebreaker({ id, teamName }) {
+  return getSpace(SPACE_ID)
+    .then((space) => {
+      return getEnvironment("master", space);
+    })
+    .then((environment) => {
+      return getEntry(id, environment);
+    })
     .then((entry) => {
-      entry.fields.title["en-US"] = "New entry title";
+      if (!entry.fields.team) {
+        entry.fields.team = { "en-US": [teamName] };
+      } else {
+        entry.fields.team["en-US"] = [...entry.fields.team["en-US"], teamName];
+      }
+
       return entry.update();
     })
     .then((entry) => console.log(`Entry ${entry.sys.id} updated.`))
     .catch(console.error);
-};
+}
 
-const getIcebreakers = async function () {
-  return client.getSpace(SPACE_ID).then((space) => {
-    // This API call will request an environment with the specified ID
-    return space.getEnvironment("master").then((environment) => {
-      // Now that we have an environment, we can get entries from that space
-      return environment.getEntries().then((entries) => {
-        return entries.items.map((replies) => {
-          return replies.fields.text["en-US"];
-        });
+async function getIcebreakers(teamName = "") {
+  return getSpace(SPACE_ID)
+    .then((space) => {
+      return getEnvironment("master", space);
+    })
+    .then((environment) => {
+      return getEntries(environment);
+    })
+    .then(({ items }) => {
+      // return the first iceBreaker that doesnt have a team value that matches the teamName passed in
+      const validIceBreakers = items.filter((item) => {
+        if (!item.fields.team) {
+          return true;
+        }
+        return !item.fields.team["en-US"].includes(teamName);
       });
-    });
-  });
-};
+
+      if (Array.isArray(validIceBreakers) && validIceBreakers.length === 0) {
+        return "I'm sorry, there are no more icebreakers for your team";
+      }
+
+      const {
+        fields: { text },
+        sys: { id },
+      } = getArrayRandomElement(validIceBreakers);
+
+      if (teamName) {
+        updateIcebreaker({ id, teamName });
+      }
+
+      return { text, id };
+    })
+    .catch(console.error);
+}
 
 module.exports = {
   updateIcebreaker,
--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const {getIcebreakers} = require('./contentfull-api');
 
 // defining the Express app
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+// adding a simple rate limiter to avoid bots
+app.use(limiter)
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -18,10 +27,11 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan("combined"));
 
-// defining an endpoint to return all ads
+// defining an endpoint to return an icebreaker
 app.get("/", async (req, res) => {
   // This API call will request a space with the specified ID
-  const results = await getIcebreakers();
+  console.log(req.query)
+  const results = await getIcebreakers(req.query.teamName);
   res.send(results)
 });
 
